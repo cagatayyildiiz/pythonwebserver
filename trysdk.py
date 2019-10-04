742709c0cebc309139699c8bd6aa0953e2af817a
cagatayyildiiz <32266801+cagatayyildiiz@users.noreply.github.com>
2019-10-05 00:36:45 +0300

    Update trysdk.py

--- trysdk.py
+++ trysdk.py
@@ -1,4 +1,3 @@
-import cursor as cursor
 import requests
 import sqlite3
 import pandas as pd
@@ -7,14 +6,19 @@ import tornado.web
 import urllib
 import datetime
 from tornado import httpclient
-BOOLA=True
 import os
 class Controller:
     def controling(fonKodu,sqlconnect):
-        connectionstr="C:\\Users\\Çağatay Yıldız\\Desktop\\New folder\\PortfoyDegerleri{0}".format(sqlconnect)
+        print(sqlconnect)
+        now = datetime.date.today()
+        intervalint = int(sqlconnect)
+        needed = datetime.date.today() - datetime.timedelta(intervalint * 365 / 12)
+        print(needed)
+        sqlquery="select * from PortfoyDegerleri WHERE FonKodu='{0}' AND Tarih >={1}".format(fonKodu,needed)
+        print(sqlquery)
+        connectionstr="C:\\Users\\Çağatay Yıldız\\Desktop\\New folder\\PortfoyDegerleri36"
         conn = sqlite3.connect(connectionstr)
         # df.to_sql(name='PortfoyDegerleri3',con=conn)
-        sqlquery = 'select * from PortfoyDegerleri WHERE FonKodu="%s"'%(fonKodu)
         df = pd.DataFrame(pd.read_sql(sqlquery, conn))
         post_data = (
                 df[["Tarih", "BirimPayDegeri"]]
@@ -22,8 +26,7 @@ class Controller:
             )  # A dictionary of your post data
         return post_data
 
-
-    def OptionController(sqlconnect):
+    def OptionController(sqlconnect,searching):
         connectionstr="C:\\Users\\Çağatay Yıldız\\Desktop\\New folder\\PortfoyDegerleri{0}".format(sqlconnect)
         conn = sqlite3.connect('C:\\Users\\Çağatay Yıldız\\Desktop\\New folder\\PortfoyDegerleri3')
         df = pd.DataFrame(pd.read_sql("SELECT DISTINCT FonKodu,FonUnvani FROM PortfoyDegerleri", conn))
@@ -36,24 +39,38 @@ class Controller:
             key = str(key).replace("'", "")
             a = ((key.split(","))[0].split(":"))[1].replace(" ", "")
             b = ((key.split(","))[1].split(":")[1]).replace("}","")
-            dictA[a] = b
+            if searching==1 or searching==" ":
+                dictA[a] = b
+            elif a.startswith(searching) and isinstance(searching,str) and searching!="":
+                dictA[a] = b
         return dictA
 
 class Main(tornado.web.RequestHandler):
-
     def get(self):
         vara = self.get_cookie("mycookie")
-        self.render("new2.html",title='TTT',dict=Controller.OptionController(vara))
+        self.render("new2.html",title='TTT',dict=Controller.OptionController(vara,1))
 class DataHandler(tornado.web.RequestHandler):
     def post(self):
         variable=self.get_argument("option")
         vara = self.get_cookie("mycookie")
         self.write({"pay": Controller.controling(variable,vara)})
+class DropdownController(tornado.web.RequestHandler):
+    def get(self):
+        self.render("tryyy.html")
+        variable2 = self.get_argument("keyvalue")
+        print(variable2)
+class Try(tornado.web.RequestHandler):
+    def post(self):
+        variable2 = self.get_argument("keyvalue")
+        print(variable2)
+        self.write(Controller.OptionController("3",variable2))
 
 def make_app():
     return tornado.web.Application([
         (r"/data", DataHandler),
         (r"/",Main),
+        (r"/dropdowncotroller",DropdownController),
+        (r"/ddd",Try)
     ])
 if __name__ == "__main__":
     app = make_app()
